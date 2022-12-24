d9725750c365d1f3ec6cb24ac76703b80bfcd369
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2022-12-24 10:49:38 +0300

    Time: 33 ms (95.10%), Space: 13.8 MB (98.43%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,15 +1,16 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        expanded1 = []
-        for i in range(len(num1)):
-            expanded1.append((num1[i], pow(10, len(num1)-i-1)))
+#         expanded1 = []
+#         for i in range(len(num1)):
+#             expanded1.append((num1[i], pow(10, len(num1)-i-1)))
         
-        expanded2 = []
-        for i in range(len(num2)):
-            expanded2.append((num2[i], pow(10, len(num2)-i-1)))
+#         expanded2 = []
+#         for i in range(len(num2)):
+#             expanded2.append((num2[i], pow(10, len(num2)-i-1)))
             
-        res = 0
-        for i1, multiplier1 in expanded1:
-            for i2, multiplier2 in expanded2:
-                res += ((int(i1)*int(i2))*int(multiplier1)*int(multiplier2))
-        return str(res)
+#         res = 0
+#         for i1, multiplier1 in expanded1:
+#             for i2, multiplier2 in expanded2:
+#                 res += ((int(i1)*int(i2))*int(multiplier1)*int(multiplier2))
+#         return str(res)
+            return str(int(num1)*int(num2))
