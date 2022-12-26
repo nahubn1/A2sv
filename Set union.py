36f2fbcfb800dc2cc2c1df90036d90e0ab8ef0b9
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2022-12-26 18:31:40 +0300

    Create Set union.py

---
+++ Set union.py
@@ -0,0 +1,8 @@
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+nE = int(input())
+setE = set(input().split(' '))
+nF = int(input())
+setF = set(input().split(' '))
+
+print(len(setE.union(setF)))
