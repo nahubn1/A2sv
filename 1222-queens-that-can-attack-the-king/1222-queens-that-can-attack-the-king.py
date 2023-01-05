bbc34f8bbcad333769962f7f25ff58bd4d066c58
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-01-05 20:37:05 +0300

    Time: 100 ms (7.08%), Space: 13.8 MB (90.27%) - LeetHub

---
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -0,0 +1,19 @@
+from itertools import product
+from copy import deepcopy
+class Solution:
+    def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
+        queens = set(map(tuple, queens))
+        directions = product([-1, 0, 1], [-1, 0, 1])
+        killers = []
+        for direc in directions:
+            ray = deepcopy(king)
+            
+            if direc != (0, 0):
+                while 0 <= ray[0]+direc[0] <= 8 and 0 <= ray[1]+direc[1] <= 8:
+                    ray[0] += direc[0]
+                    ray[1] += direc[1]
+                    
+                    if tuple(ray) in queens:
+                        killers.append(deepcopy(ray))
+                        break
+        return killers
