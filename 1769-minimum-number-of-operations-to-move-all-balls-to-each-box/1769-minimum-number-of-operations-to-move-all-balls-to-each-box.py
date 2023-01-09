90d2fe4559ef43fdd9b8c5fc8258dba8f508ce32
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-01-09 16:39:17 +0300

    Time: 72 ms (92.19%), Space: 14.2 MB (85.51%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -2,33 +2,27 @@ class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         n = len(boxes)
         
-        balls_before = []
-        for i in range(n):
-            if balls_before:
-                balls_before.append(balls_before[-1]+int(boxes[i-1]))
-            else:
-                balls_before.append(0)
-        
-        balls_after = []
-        for i in range(n-1, -1, -1):
-            if balls_after:
-                balls_after.append(balls_after[-1]+int(boxes[i+1]))
-            else:
-                balls_after.append(0)
-        
+        balls_before = balls_after = 0
+
         moves_right = []
         for i in range(n):
             if moves_right:
-                moves_right.append(moves_right[-1]+balls_before[i])
+                moves_right.append(moves_right[-1]+balls_before)
             else:
-                moves_right.append(balls_before[i])
+                moves_right.append(balls_before)
+                
+            if boxes[i] == '1':
+                balls_before += 1
                 
         moves_left = []
-        for i in range(n-1, -1, -1):
+        for i in range(n-1, -1, -1):                
             if moves_left:
-                moves_left.append(moves_left[-1]+balls_after[n-i-1])
+                moves_left.append(moves_left[-1]+balls_after)
             else:
-                moves_left.append(balls_after[n-i-1])
+                moves_left.append(balls_after)
+                
+            if boxes[i] == '1':
+                balls_after += 1
                 
         moves = []
         for i in range(n):
