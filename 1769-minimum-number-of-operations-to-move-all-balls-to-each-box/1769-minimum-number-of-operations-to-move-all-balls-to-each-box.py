cc19f31f47a21112c6bcea0cbf24c3b426bf00fe
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-01-09 16:23:59 +0300

    Time: 93 ms (81.68%), Space: 14.6 MB (7.24%) - LeetHub

---
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        n = len(boxes)
+        
+        balls_before = []
+        for i in range(n):
+            if balls_before:
+                balls_before.append(balls_before[-1]+int(boxes[i-1]))
+            else:
+                balls_before.append(0)
+        
+        balls_after = []
+        for i in range(n-1, -1, -1):
+            if balls_after:
+                balls_after.append(balls_after[-1]+int(boxes[i+1]))
+            else:
+                balls_after.append(0)
+        balls_after.reverse()
+        
+        moves_right = []
+        for i in range(n):
+            if moves_right:
+                moves_right.append(moves_right[-1]+balls_before[i])
+            else:
+                moves_right.append(balls_before[i])
+                
+        moves_left = []
+        for i in range(n-1, -1, -1):
+            if moves_left:
+                moves_left.append(moves_left[-1]+balls_after[i])
+            else:
+                moves_left.append(balls_after[i])
+                
+        moves = []
+        for i in range(n):
+            moves.append(moves_right[i]+moves_left[n-i-1])
+            
+        return moves
