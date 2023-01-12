f14861d9327764715b6f117ad67e1bae9aafb423
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-01-12 18:39:55 +0300

    Time: 221 ms (69.83%), Space: 14.8 MB (23.40%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -1,14 +1,14 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        deleted = set()
-        m, n = len(strs), len(strs[0])
-        last_ascii = [96]*n
-        for string in strs:
-            for i in range(n):
-                if i not in deleted:
-                    if ord(string[i]) < last_ascii[i]:
-                        deleted.add(i)
-                    else:
-                        last_ascii[i] = ord(string[i])
-        
-        return len(deleted)
+        deleted = 0
+        for j in range(len(strs[0])):
+            column_order = 0
+            for i in range(len(strs)):
+                if ord(strs[i][j]) < column_order:
+                    print(chr(column_order), strs[i][j])
+                    deleted += 1
+                    break
+                else:
+                    column_order = ord(strs[i][j])
+                    
+        return deleted
