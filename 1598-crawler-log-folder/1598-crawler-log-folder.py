583be72e9415573259711e076b26bc3d8ed56d01
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-02-24 11:32:30 +0300

    Time: 45 ms (81.79%), Space: 14.1 MB (83.82%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -3,15 +3,10 @@ class Solution:
         stack = []
         for log in logs:
             opr = log.split('/')[0]
-            # print(opr)
             if stack and opr == '..':
-                # print('>')
                 stack.pop()
             elif opr != '.' and opr != '..':
-                # print('>>')
                 stack.append(opr)
-            # print('______')
         
-        # print(stack)
         return len(stack)
         
