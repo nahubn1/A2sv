6ac82085fe5abbf5fab8fa585d3f2f21905b7796
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-03-02 11:35:45 +0300

    added reverse-string.python

---
+++ reverse-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def swap(self, start, end):
+        if start >= end:
+            return
+
+        self.s[start], self.s[end] = self.s[end], self.s[start] 
+        self.swap(start+1, end-1)
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        self.s = s
+        self.swap(0, len(s)-1)
