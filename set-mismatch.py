3d9b7ec517b87cb35cfca1da6cd12f3b3b8311f5
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-03-24 19:47:11 +0300

    added set-mismatch.python

---
+++ set-mismatch.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        rep = None
+        i = 0
+        while i < len(nums):
+            if i == nums[i]-1 or nums[i] == 0:
+                i += 1
+            else:
+                if nums[i] != nums[nums[i]-1]:
+                    nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+                else:
+                    rep = nums[i]
+                    nums[i] = 0
+        
+        return [rep, nums.index(0)+1]
