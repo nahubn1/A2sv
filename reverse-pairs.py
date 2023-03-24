61f950b916263beb43eb4f759af991003c5ec482
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-03-25 00:56:48 +0300

    added reverse-pairs.python

--- reverse-pairs.py
+++ reverse-pairs.py
@@ -1,21 +1,35 @@
+from sortedcontainers import SortedList
 class Solution:
+    @staticmethod
+    def merge(arr1, arr2):
+        ptr1, ptr2  = 0, 0
+        res = []
+        while ptr1 < len(arr1) and ptr2 < len(arr2):
+            if arr1[ptr1] < arr2[ptr2]:
+                res.append(arr1[ptr1])
+                ptr1 += 1
+            else:
+                res.append(arr2[ptr2])
+                ptr2 += 1
+        
+        return res + arr1[ptr1:] + arr2[ptr2:]
+
     def reversePairs(self, nums: List[int]) -> int:
         self.count = 0
-        def sort(start, arr):
-            if start == len(nums)-1:
-                return 
 
-            num = arr[start]
-            sort(start+1, nums)
+        def mergeSort(start, end):
+            if start == end:
+                return [nums[start]]
+            
+            middle = (start+end)//2
+            left = mergeSort(start, middle)
+            right = mergeSort(middle+1, end)
 
-            self.count += (bisect_left(arr, num/2, lo=start+1) - start - 1)
-
-            i = bisect_right(arr, num, lo=start)
-            arr.insert(i, num)
-            arr.pop(start)
-                
-                
+            for num in left:
+                self.count += bisect_left(right, num/2) 
+            
+            return self.merge(left, right)
         
-        sort(0, nums)
+        mergeSort(0, len(nums)-1)
 
         return self.count
