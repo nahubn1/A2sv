d447f1fd38351426beb65238527f058cb0c79ec1
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-05-04 16:01:06 +0300

    added heap sort.py

---
+++ heap sort.py
@@ -0,0 +1,29 @@
+class Solution:
+    #Heapify function to maintain heap property.
+    def heapify(self,arr, n, i):
+        
+        largest = i
+        left = 2*i + 1
+        right = 2*i + 2
+        if left < n and arr[left] > arr[largest]:
+            largest = left
+        if right < n and arr[right] > arr[largest]:
+            largest = right
+        
+        if largest != i:
+            arr[largest], arr[i] = arr[i], arr[largest]
+            self.heapify(arr, n, largest)
+        
+    #Function to build a Heap from array.
+    def buildHeap(self,arr,n):
+        for i in range(n//2 - 1, -1, -1):
+            self.heapify(arr, n, i)
+            
+            
+    
+    #Function to sort an array using Heap Sort.    
+    def HeapSort(self, arr, n):
+        self.buildHeap(arr, n)
+        for i in range(len(arr)-1, 0, -1):
+            arr[0], arr[i] = arr[i], arr[0]
+            self.heapify(arr, i, 0)
