11a0dd7c0fc6e90cb9bd23a1fa8e5b035c9b4ba2
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-10-06 13:23:13 -0700

    added evaluate-division.python

--- evaluate-division.py
+++ evaluate-division.py
@@ -1,36 +1,26 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        ratio = defaultdict(lambda: 1)
-        rep = {}
-        for a, b in equations: 
-            rep[a] = a
-            rep[b] = b
-     
-        def find(x):
-            if rep[x] == x:
-                return x
-            
-            temp = rep[x]
-            rep[x] = find(rep[x])
-            ratio[x] *= ratio[temp]
-            return rep[x]
+        
+        ratio = defaultdict(lambda: -1)
+        
+        variables = set()
+        for (u, v), val in zip(equations, values):
+            variables.add(u)
+            variables.add(v)
 
-        def union(a, b, val):
-            rep_a, rep_b = find(a), find(b)
+            ratio[(u, u)] = ratio[(v, v)] = 1
+            ratio[(u, v)] = val
+            ratio[(v, u)] = 1/val
+        
 
-            if rep_a!= rep_b:
-                rep[rep_a] = rep_b
-                ratio[rep_a] = val / (ratio[a] / ratio[b])
-
-        for (a, b), val in zip(equations, values):
-            union(a, b, val)
-
+        for c in variables:
+            for a in variables:
+                for b in variables:
+                    if ratio[(a, c)] != -1 and ratio[(b, c)] != -1:
+                        ratio[(a, b)] = ratio[(a, c)] / ratio[(b, c)]
+        
         ans = []
         for u, v in queries:
-            
-            if u not in rep or v not in rep or find(u) != find(v):
-                ans.append(-1)
-            else:
-                ans.append(ratio[u] / ratio[v])    
-
+            ans.append(ratio[(u, v)])
+        
         return ans
