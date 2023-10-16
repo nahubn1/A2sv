b4b4567904754cd3b47d42afbb1cb8e78fcea909
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-10-16 05:35:21 -0700

    added unique-paths.python

--- unique-paths.py
+++ unique-paths.py
@@ -1,9 +1,3 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        grid = [[1]*n for i in range(m)]
-        print(grid)
-        for i in range(1, m):
-            for j in range(1, n):
-                grid[i][j] = grid[i-1][j] + grid[i][j-1]
-        
-        return grid[-1][-1]
+        return comb(m+n-2, m-1)
