9f8e1f1b27b3aa3975687bd0865ddd65a72d28f1
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-10-19 12:02:36 +0300

    added lexicographically-smallest-equivalent-string.python

--- lexicographically-smallest-equivalent-string.py
+++ lexicographically-smallest-equivalent-string.py
@@ -1,23 +1,29 @@
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        rep = {chr(i): chr(i) for i in range(97, 123)}
-
-        def find(char):
-            if rep[char] == char:
-                return char
-            
-            rep[char] = find(rep[char])
-            return rep[char]
-
-        def union(char1, char2):
-            r_char1, r_char2 = find(char1), find(char2)
-    
-            if r_char1 < r_char2:
-                rep[r_char2] = r_char1
-            elif r_char1 > r_char2:
-                rep[r_char1] = r_char2
-    
-        for c1, c2 in zip(s1, s2):
-            union(c1, c2)
         
-        return ''.join([find(c) for c in baseStr])
+        rep = {chr(i): chr(i) for i in range(97, 123)}
+        def find(x):
+            if x == rep[x]:
+                return x
+            
+            rep[x] = find(rep[x])
+            return rep[x]
+        
+        def union(u, v):
+            repU = find(u)
+            repV = find(v)
+
+            if repU != repV:
+                if repU < repV:
+                    rep[repV] = repU
+                else:
+                    rep[repU] = repV
+        
+        for char1, char2 in zip(s1, s2):
+            union(char1, char2)
+
+        ans = []
+        for char in baseStr:
+            ans.append(find(char))
+
+        return ''.join(ans)
